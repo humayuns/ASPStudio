bfa9b4e0ae1cce2ac16afe755ef3ad136b151dc4
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-06-23 02:23:59 +0500

    added code to load treeview

--- ASPStudio/Form1.Designer.vb
+++ ASPStudio/Form1.Designer.vb
@@ -23,26 +23,61 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'TreeView1
         '
-        Me.TreeView1.Location = New System.Drawing.Point(12, 12)
+        Me.TreeView1.Location = New System.Drawing.Point(12, 38)
         Me.TreeView1.Name = "TreeView1"
-        Me.TreeView1.Size = New System.Drawing.Size(208, 477)
+        Me.TreeView1.Size = New System.Drawing.Size(386, 451)
         Me.TreeView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(404, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Parse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(404, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(385, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(140, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "List of files in provided folder"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(801, 501)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TreeView1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "ASPStudio - Includes Mapper"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 
 End Class
--- ASPStudio/Form1.vb
+++ ASPStudio/Form1.vb
@@ -1,3 +1,28 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+
+        For Each f In IO.Directory.GetFiles(TextBox1.Text)
+
+
+            Dim newnode = TreeView1.Nodes.Add(f.Replace(TextBox1.Text, ""))
+
+            Dim filetext = IO.File.ReadAllText(f)
+
+            If filetext.Contains("#include") Then
+                newnode.Nodes.Add("includes")
+            End If
+        Next
+
+
+    End Sub
+
+
+    Function GetFileText(filename As String) As String
+        Return IO.File.ReadAllText(filename)
+    End Function
+
+    
 End Class
