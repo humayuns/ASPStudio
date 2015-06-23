f4e65207f22f0bae65261d23854ea4a2cf022486
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-06-24 03:17:00 +0500

    added StringFunctions module

---
+++ ASPStudio/StringFunctions.vb
@@ -0,0 +1,9 @@
+﻿Module StringFunctions
+
+
+    Function CountSubString(text As String, search As String) As Integer
+        Dim Occurrences As Integer = (text.Length - text.Replace(search, String.Empty).Length) / search.Length
+        Return Occurrences
+    End Function
+
+End Module
