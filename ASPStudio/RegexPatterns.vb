c96253678c85fabb1f41f4fbc7a3c8f5b67c1fad
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-07-02 20:24:13 +0500

    added IsEmail function

--- ASPStudio/RegexPatterns.vb
+++ ASPStudio/RegexPatterns.vb
@@ -1,4 +1,6 @@
-﻿Module RegexPatterns
+﻿Imports System.Text.RegularExpressions
+
+Module RegexPatterns
 
 
 
@@ -8,6 +10,10 @@
     ' ^\s*(Sub.*?)^\s*End Sub
     ' Sub SubName[\s\S]*?End Sub
 
+    Function IsEmail(ByVal email As String) As Boolean
+        Static emailExpression As New Regex("^[_a-z0-9-]+(.[a-z0-9-]+)@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$")
 
+        Return emailExpression.IsMatch(email)
+    End Function
 
 End Module
