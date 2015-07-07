8a549f1cd16335e4f5beb54e9cdd4cd986c92973
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-07-08 04:55:55 +0500

    added asp utils module

---
+++ ASPStudio/ASPUtils.vb
@@ -0,0 +1,13 @@
+﻿Module ASPUtils
+
+
+    Function MergeIncludes(text As String) As String
+        Return ""
+    End Function
+
+    Function GetFunctions(code As String) As List(Of String)
+        Return New List(Of String)
+    End Function
+
+
+End Module
